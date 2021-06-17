c66b734686a1642e179929aacdc90af5debfef67
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-06-18 00:00:57 +0900

    21.06.18 math

--- backjoon/math/4153_직각삼각형.py
+++ backjoon/math/4153_직각삼각형.py
@@ -1,11 +1,12 @@
 check = True
 while check:
     data = list(map(int, input().split()))
+    if data.count(0) == 3:
+        check = False
+        break
     data.sort(reverse=True)
-    bigV, s1, s2 = data
-    if bigV ** 2 == s1**2+s2**2:
+    bigV, s1, s2 = data[0], data[1], data[2]
+    if bigV ** 2 == s1**2+s2**2 and bigV > 0 and s1 > 0 and s2 > 0:
         print("right")
     else:
         print("wrong")
-    if data.count(0) == 3:
-        check = False
