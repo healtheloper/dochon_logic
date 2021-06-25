722639e10f5e63fc4ac35f568e6c8faab7a7bd7b
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-06-25 23:57:35 +0900

    21.06.25

--- backjoon/string/4659_비밀번호발음.py
+++ backjoon/string/4659_비밀번호발음.py
@@ -1,35 +1,40 @@
 while True:
-    try:
-        data = input()
-        vowel = ["a","e","i","o","u"]
-        cur_char = ''
-        cur_count = 0
-        conso_count = 0
-        vo_count = 0
-        vo_count_all = 0
-        isAccept = True
-        for char in data:
-            print(char, cur_char)
-            if char not in vowel:
-                conso_count += 1
-                vo_count = 0
-            elif char in vowel:
-                vo_count += 1
-                vo_count_all += 1
-                conso_count = 0
-            elif char == cur_char and cur_char != "o" and cur_char != "e":
-                print(char, cur_char)
-                isAccept = False
-                break
-            elif (cur_char == "e" or cur_char == "o") and cur_count < 2:
-                continue
-            if conso_count == 3 or vo_count == 3:
-                isAccept = False
-                break
-            cur_char = char
-        if isAccept and vo_count_all != 0:
-            print(f"<{data}> is acceptable")
-        else:
-            print(f"<{data}> is not acceptable")
-    except:
+    data = input()
+    if data == "end":
         break
+    vowel = ["a","e","i","o","u"]
+    cur_char = ''
+    cur_count = 0
+    conso_count = 0
+    vo_count = 0
+    vo_count_all = 0
+    isAccept = True
+    for char in data:
+        if char not in vowel:
+            conso_count += 1
+            vo_count = 0
+        elif char in vowel:
+            vo_count += 1
+            vo_count_all += 1
+            conso_count = 0
+        if char == cur_char and (cur_char != "o" and cur_char != "e"):
+            if cur_char == "e" or cur_char == "o":
+                if cur_count < 2:
+                    continue
+                else:
+                    isAccept = False
+                    break
+            else:
+                isAccept = False
+                break
+
+        if conso_count == 3 or vo_count == 3:
+            isAccept = False
+            break
+
+        cur_char = char
+
+    if isAccept and vo_count_all != 0:
+        print(f"<{data}> is acceptable")
+    else:
+        print(f"<{data}> is not acceptable")
