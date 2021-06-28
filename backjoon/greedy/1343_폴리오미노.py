a4659a07160346ca966ad588f70fb4ff4bf67d52
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-06-28 23:56:54 +0900

    21.06.28 greedy

---
+++ backjoon/greedy/1343_폴리오미노.py
@@ -0,0 +1,28 @@
+from abc import abstractproperty
+
+
+data = input().split(".")
+check = True
+arr = []
+while check:
+    for item in data:
+        total_len = len(item)
+        if total_len == 0:
+            arr.append(".")
+            continue
+        while total_len > 0:
+            if total_len % 2 == 1:
+                print(-1)
+                check = False
+                break
+            if total_len > 3:
+                total_len -= 4
+                arr.append("AAAA")
+            elif total_len > 1:
+                total_len -= 2
+                arr.append("BB")
+        arr.append(".")
+    break
+arr.pop()
+if check == True:
+    print(''.join(arr))
