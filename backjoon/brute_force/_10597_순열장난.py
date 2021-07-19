ebf01cda66432d841172c66825cb1100a8c2d58f
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-07-19 23:23:59 +0900

    21.07.19

---
+++ backjoon/brute_force/_10597_순열장난.py
@@ -0,0 +1,14 @@
+data = input()
+arr = []
+long_val = ''
+print(data)
+for idx in range(len(data)):
+    in_val = data[idx]
+    if not in_val in arr and long_val == '':
+        arr.append(in_val)
+    else:
+        long_val += data[idx]
+        if not long_val in arr:
+            arr.append(long_val)
+            long_val = ''
+print(' '.join(arr))
