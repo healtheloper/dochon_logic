f001b0e35bf699f4eadb0b5479d257a6d11b585f
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-11-03 21:27:55 +0900

    21.11.03 backjoon 20437 js

---
+++ backjoon/string/20437_문자열게임2.js
@@ -0,0 +1,53 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+
+const solution = (input) => {
+  let answer = [];
+  const [T, ...rest] = input;
+  for (let i = 0; i < T; i++) {
+    let min = 1e9;
+    let max = -1;
+    const idx = i * 2;
+    const str = rest[idx];
+    const str_dict = {};
+    const k = +rest[idx + 1];
+    for (let x = 0; x < str.length; x++) {
+      if (str_dict[str[x]]) str_dict[str[x]]++;
+      else str_dict[str[x]] = 1;
+    }
+    for (let j = 0; j < str.length; j++) {
+      const char = str[j];
+      if (str_dict[char] < k) continue;
+      let charCount = 0;
+      for (let z = j; z < str.length; z++) {
+        const compareChar = str[z];
+        if (char === compareChar) {
+          charCount++;
+          if (charCount === k) {
+            min = Math.min(z - j + 1, min);
+            max = Math.max(z - j + 1, max);
+            charCount = 1;
+            break;
+          }
+        }
+      }
+    }
+    if (min !== 1e9) {
+      answer.push(`${min} ${max}`);
+    } else {
+      answer.push("-1");
+    }
+  }
+  return answer.join("\n");
+};
+
+rl.on("line", (answer) => {
+  input.push(answer.trim());
+}).on("close", () => {
+  console.log(solution(input));
+});
