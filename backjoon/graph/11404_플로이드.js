00bb1b93c8e2c592be04a943e08cae84722d7e11
healtheloper <58503584+healtheloper@users.noreply.github.com>
2021-11-12 21:13:39 +0900

    21.11.12 backjoon 11404 js

---
+++ backjoon/graph/11404_플로이드.js
@@ -0,0 +1,34 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const input = [];
+
+const solution = (input) => {
+  let answer = 0;
+  const [n, m, ...infos] = input;
+  const array = new Array(+n)
+    .fill(null)
+    .map((v) => new Array(+n).fill(Infinity));
+  infos.forEach((info) => {
+    const [a, b, c] = info.split(" ");
+    array[+a - 1][+b - 1] = Math.min(array[+a - 1][+b - 1], +c);
+  });
+  for (let i = 0; i < +n; i++) {
+    for (let j = 0; j < +n; j++) {
+      for (let k = 0; k < +n; k++) {
+        if (j == k) array[j][k] = 0;
+        array[j][k] = Math.min(array[j][k], array[j][i] + array[i][k]);
+      }
+    }
+  }
+  return array.map((v) => v.join(" ")).join("\n");
+};
+
+rl.on("line", (answer) => {
+  input.push(answer.trim());
+}).on("close", () => {
+  console.log(solution(input));
+});
