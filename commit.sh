a335be9ef8c700c5f455d43c6cd443e1f45e48ea
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-09-08 13:22:56 +0900

    feat: commit message format - platform

--- commit.sh
+++ commit.sh
@@ -12,10 +12,10 @@ echo "> ✅ add all files"
 if [ $# -eq 0 ]; then
     read -p "> 💻 enter commit message : " COMMIT_MESSAGE
 else
-    COMMIT_MESSAGE=${1}
+    COMMIT_MESSAGE=${2}
 fi
 
-git commit -m "$today ${COMMIT_MESSAGE}"
+git commit -m "$today [$1] ${COMMIT_MESSAGE}"
 
 echo "> ✅ commit complete"
 
